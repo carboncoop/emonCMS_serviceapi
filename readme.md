4693eea05ba0ff412b23e55a9791fd421bc23c84
cagabi <carlos@carbon.coop>
2019-04-14 18:55:20 +0100

    Service API module

---
+++ readme.md
@@ -0,0 +1,31 @@
+Emoncms is an open-source web application for processing, logging and visualising energy, 
+temperature and other environmental data and is part of the [OpenEnergyMonitor project](
+http://openenergymonitor.org).
+
+#ServiceAPI module
+Get access to user's data in an emonCMS installation by providing a master apikey (aka service_apikey)
+and the username or email address of the user.
+The service_apikey can be sent in the query string of the URL or as a POST field (recommended)
+
+##Example
+The address to use would look like
+```
+https://youremoncms.org/feed/list.json?service_apikey=the_key_in_your_settings&usernameaccess=a_user_name
+https://youremoncms.org/feed/list.json?service_apikey=the_key_in_your_settings&emailaccess=a_user_email
+```
+
+## License
+This module is released under the GNU Affero General Public License
+
+##Installation
+Generate a 32 bit apikey by running `openssl rand -hex 32`. 
+Copy the generated key into settings.php:
+```
+$service_apikey = "YOUR_APIKEY"
+```
+
+Grant Write or Read API access in settings.php:
+```
+$serviceapi_mode = "write || read"; // if omitted it will be "read" by default
+```
+
---
+++ serviceapi_controller.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+  All Emoncms code is released under the GNU Affero General Public License.
+  See COPYRIGHT.txt and LICENSE.txt.
+
+  ---------------------------------------------------------------------
+  Emoncms - open source energy visualisation
+  Part of the OpenEnergyMonitor project:
+  http://openenergymonitor.org
+
+ */
+
+// no direct access
+defined('EMONCMS_EXEC') or die('Restricted access');
+
+function serviceapi_controller() {
+
+    global $user, $route, $path, $serviceapi_apikey, $serviceapi_mode;
+
+    $result = false;
+
+    if (prop('service_apikey') === $serviceapi_apikey) {
+
+        if ($route->format !== 'json') {
+            http_response_code(400);
+            return "400 bad request";
+        }
+
+        $userid = 0;
+
+        // 1. Get userid
+        $username = prop('usernameaccess');
+        if ($username)
+            $userid = $user->get_id($username);
+        else {
+            $user_email = prop('emailaccess');
+            if ($user_email) {
+                $users = $user->get_usernames_by_email($user_email);
+                $userid = $users[0]['id'];
+            }
+        }
+
+        if ($userid > 0) {
+            // 2. Get apikey
+            if ($serviceapi_mode === "write")
+                $apikey = $user->get_apikey_write($userid);
+            else
+                $apikey = $user->get_apikey_read($userid);
+
+            // 3. Prepare curl request
+            $URL = $path . str_replace("serviceapi/", '', get('q'));
+            foreach ($_GET as $key => $value) {
+                if ($key != "service_apikey" && $key != "q")
+                    $URL .= "&" . $key . '=' . $value;
+            }
+            $ch = curl_init($URL);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            $post_fields = 'apikey=' . $apikey;
+            foreach ($_POST as $key => $value) {
+                if ($key != "service_apikey")
+                    $post_fields .= "&" . $key . '=' . $value;
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+
+            // 4. execute curl request
+            curl_exec($ch);
+            curl_close($ch);
+            die();
+        }
+    }
+
+    if ($result === false) {
+        http_response_code(401);
+        return array('content' => "401 Unauthorized",);
+    }
+}
